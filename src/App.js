f7050c39c49f1a5057d349007c6707c3e81b1ef8
André Bonafé <andre.bonaf@gmail.com>
2021-12-09 16:22:53 -0300

    feito tela de login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Pages/Login';
+import Meals from './Pages/Meals';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ Meals } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
