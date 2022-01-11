0b78c3c32c78ad43b8600b752d06a33dc5006968
André Bonafé <andre.bonaf@gmail.com>
2022-01-11 16:56:55 -0300

    Tela de Perfil concluida

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
-export default function Profile() {
+export default function Profile(props) {
+  const { history } = props;
+
+  const [User, setUser] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem('user') !== null) {
+      setUser(JSON.parse(localStorage.getItem('user')).email);
+    }
+  }, []);
+
   return (
     <div>
       <Header pageName="Perfil" showSerachIcon={ false } />
+      <h1 data-testid="profile-email">{User}</h1>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.any,
+}.isRequired;
